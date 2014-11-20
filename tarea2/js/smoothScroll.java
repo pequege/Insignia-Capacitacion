c5f75deadaed6badfa2ce702687204a26e3458aa
PQG <gerardoemansilla@gmail.com>
2014-11-20 00:51:09 -0300

    tarea2.1
    fixed some css issues

--- tarea2/js/smoothScroll.java
+++
@@ -1,8 +0,0 @@
-/* Smooth scrolling para anclas */
-$('a.smooth').live('click', function(e) {
-    var $link = $(this);
-    var anchor  = $link.attr('href');
-    $('html, body').stop().animate({
-        scrollTop: $(anchor).offset().top
-    }, 1000);
-});
